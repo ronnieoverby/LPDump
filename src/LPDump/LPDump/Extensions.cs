1252ea94254c7feff5637659a7dfefe8e10e10d7
Ronnie Overby <roverby@core-techs.net>
2012-07-31 13:52:41 -0400

    seems to work

---
+++ src/LPDump/LPDump/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace LPDump
+{
+    public static class Extensions
+    {
+        public static T Dump<T>(this T source)
+        {
+            var localUrl = Path.GetTempFileName() + ".html";
+            using (var writer = LINQPad.Util.CreateXhtmlWriter(true))
+            {
+                writer.Write(source);
+                File.WriteAllText(localUrl, writer.ToString());
+            }
+            Process.Start(localUrl);
+            return source;
+        }
+    }
+}
